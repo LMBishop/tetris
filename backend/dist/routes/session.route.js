c6c93acc32893d891adb39011674c02f3f04999e
Leonardo Bishop <me@leonardobishop.com>
2023-11-04 23:59:44 +0000

    gitignore some shit

--- backend/dist/routes/session.route.js
+++
@@ -1,6 +0,0 @@
-import { createNewSession } from "../config/session-store.js";
-export function createSession(req, res) {
-    const session = createNewSession();
-    res.status(201).send(session);
-}
-//# sourceMappingURL=session.route.js.map
